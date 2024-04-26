f5bf472b4ab69432027ce083c65e5bb8dec934a5
Vladislav Borisov <159052294+Nightwayboom@users.noreply.github.com>
2024-04-26 12:19:58 +0300

    Vladislove #3 (#6)
    * readme2
    
    * func read

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(123)
+// console.log(123)
+
+const board = [[1, 2, 3],[4,5,6]]
+console.table(board)
+console.log(board[1][1])
+
+const board2 = [board[0][0], board[0][1], board[1][0], board[1][1]]
+console.table(board2)
