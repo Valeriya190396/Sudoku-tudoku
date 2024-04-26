c4bc4b15f202b4dc6872f69b68c56a9c80d2e328
Vladislav Borisov <nightwayboom@gmail.com>
2024-04-26 12:17:19 +0300

    func read

--- sudoku.js
+++ sudoku.js
@@ -1,20 +1,29 @@
 const fs = require('fs')
-function read() {
+function read(num) {
 	const puzzles = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n')
-	const gameFirst = puzzles[0]
-	console.log(gameFirst.length)
-	/**
-	 * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-	 */
+	const gamePuzzle = puzzles[num - 1].split('')
+	const newBoard = [
+		[gamePuzzle.slice(0, 9)],
+		[gamePuzzle.slice(9, 18)],
+		[gamePuzzle.slice(18, 27)],
+		[gamePuzzle.slice(27, 36)],
+		[gamePuzzle.slice(36, 45)],
+		[gamePuzzle.slice(45, 54)],
+		[gamePuzzle.slice(54, 63)],
+		[gamePuzzle.slice(63, 72)],
+		[gamePuzzle.slice(72, 81)],
+	]
+	return newBoard
 }
+const read1 = read(1)
 
-function solve() {
+function solve(table) {
+	return table
 	/**
 	 * Принимает игровое поле в том формате, в котором его вернули из функции read.
 	 * Возвращает игровое поле после попытки его решить.
 	 */
 }
-
 function isSolved() {
 	/**
 	 * Принимает игровое поле в том формате, в котором его вернули из функции solve.
